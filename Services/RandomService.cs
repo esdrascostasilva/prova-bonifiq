1435a2babe961e4146bd994b2e1697e21fa83de9
esdrascosta <esdras.cts@outlook.com>
2025-08-15 18:41:36 -0300

    feat: resolvendo o segundo problema da controller1

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -11,8 +11,9 @@ namespace ProvaPub.Services
 		public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
+                .UseSqlServer(@"Server=localhost; Database=TesteBonifiq; User Id=sa; Password=myPassw0rd; TrustServerCertificate=True;")
+                // .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+                .Options;
             seed = Guid.NewGuid().GetHashCode();
 
             _ctx = new TestDbContext(contextOptions);
@@ -20,6 +21,15 @@ namespace ProvaPub.Services
         public async Task<int> GetRandom()
 		{
             var number =  new Random(seed).Next(100);
+
+            // preciso tratar o numero gerado para que, caso eu ja tenha no meu banco ele avise o usuario e peça pra gerar outro
+            bool exists = await _ctx.Numbers.AnyAsync(n => n.Number == number);
+
+            if (exists)
+            {
+                throw new InvalidOperationException($"O numero gerado ja existe. Por favor, gere outro");
+            }
+            
             _ctx.Numbers.Add(new RandomNumber() { Number = number });
             _ctx.SaveChanges();
 			return number;
